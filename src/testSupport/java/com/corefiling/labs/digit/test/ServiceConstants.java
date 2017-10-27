952da3fab51f7cb8a2c193b219fde69ff668f2c0
Matt Hillsdon <mth@corefiling.com>
2017-10-27 13:48:59 +0100

    More version faff.

--- src/testSupport/java/com/corefiling/labs/digit/test/ServiceConstants.java
+++ src/testSupport/java/com/corefiling/labs/digit/test/ServiceConstants.java
@@ -23,7 +23,7 @@ public final class ServiceConstants {
 
   public static final String SERVICE_NAME = "CoreFiling Labs Digit Frequency Analysis API";
 
-  public static final String API_VERSION_PREFIX = "0.1.";
+  public static final String API_VERSION_PREFIX = "1.0.";
 
   private ServiceConstants() {
   }
